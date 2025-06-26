51f9f186498a62d00abe5ff1a5f68acc95cbd96a
AcchonBurike <AcchonBurike@example.com>
2025-06-26 19:33:59 +0900

    おみくじ結果が再抽選されない不具合を修正

--- omikuji.js
+++ omikuji.js
@@ -5,13 +5,12 @@ const omikujiButton = document.querySelector("#omikuji-button");
 
 const result = ["大吉", "中吉", "吉", "末吉", "凶", "大凶"];
 
-let randomIndex = Math.floor(Math.random() * result.length);
-
 omikujiButton.addEventListener(
   'click',
   () => {
     const newPar = document.createElement("p");
+    const randomIndex = Math.floor(Math.random() * result.length);
     newPar.textContent = result[randomIndex];
     resultDivision.append(newPar);
-  },
+  }
 );
