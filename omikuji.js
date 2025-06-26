fe6524718991f3c428fdad63dcc01b390eb5e776
AcchonBurike <AcchonBurike@example.com>
2025-06-26 19:24:53 +0900

    おみくじ機能を実装

--- omikuji.js
+++ omikuji.js
@@ -1,1 +1,17 @@
-'use srtict';
+'use strict';
+
+const resultDivision = document.querySelector("#result-area");
+const omikujiButton = document.querySelector("#omikuji-button");
+
+const result = ["大吉", "中吉", "吉", "末吉", "凶", "大凶"];
+
+let randomIndex = Math.floor(Math.random() * result.length);
+
+omikujiButton.addEventListener(
+  'click',
+  () => {
+    const newPar = document.createElement("p");
+    newPar.textContent = result[randomIndex];
+    resultDivision.append(newPar);
+  },
+);
