59e89bc63f4ca9640d33d6814a5bdd324d9620d4
AcchonBurike <AcchonBurike@example.com>
2025-06-27 15:04:07 +0900

    おみくじ結果が最新のものしか表示されないように変更

--- omikuji.js
+++ omikuji.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const resultDivision = document.querySelector("#result-area");
 const omikujiButton = document.querySelector("#omikuji-button");
 
 const result = ["大吉", "中吉", "吉", "末吉", "凶", "大凶"];
@@ -8,9 +7,15 @@ const result = ["大吉", "中吉", "吉", "末吉", "凶", "大凶"];
 omikujiButton.addEventListener(
   'click',
   () => {
-    const newPar = document.createElement("p");
     const randomIndex = Math.floor(Math.random() * result.length);
-    newPar.textContent = result[randomIndex];
-    resultDivision.append(newPar);
+    const resultText = result[randomIndex];
+    const resultDivision = document.querySelector("#result-area");
+    if (document.querySelector("#result-area p")){
+      resultDivision.textContent = resultText;
+    } else {
+      const newPar = document.createElement("p");
+      newPar.textContent = resultText;
+      resultDivision.append(newPar);
+    }
   }
 );
